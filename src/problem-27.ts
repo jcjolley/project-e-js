172f5df1b8eaf9e3c673c92a793ef15e86962e96
Joshua Jolley <jjolley@clearwateranalytics.com>
2018-10-26 11:23:19 -0600

    CLeaned up for loop

--- src/problem-27.ts
+++ src/problem-27.ts
@@ -8,14 +8,14 @@ export const doProblem = () => {
     const bs = primes.takeWhile(x => x < 1000); // b must be prime b/c if n === 0, 0 + 0 + b = must be a prime
 
     let ans = { a: -999, b: -999, n: 0 };
-    for (let i = 0; i < bs.length; i++) {
-      for (let a = 2 - bs[i]; a < 1000; a += 2) { // a must be odd (otherwise half of results would be even), and a > 1 - b (n = 1, 1 + a + b >=2 b/c b must be prime)
+    for (const b of bs) {
+      for (let a = 2 - b; a < 1000; a += 2) { // a must be odd (otherwise half of results would be even), and a > 1 - b (n = 1, 1 + a + b >=2 b/c b must be prime)
         let n = 0;
-        while (isPrime((n * n) + (a * n) + bs[i])) {
+        while (isPrime((n * n) + (a * n) + b)) {
           n++;
         }
         if (n > ans.n) {
-          ans = { a, b: bs[i], n }
+          ans = { a, b: b, n }
         }
       }
     }
